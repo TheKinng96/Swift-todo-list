3b931abf6d902fa0a7561a201c023468a44a8ccd
G3N <yap@corekara.co.jp>
2022-02-06 22:35:49 +0900

    added simple plist to work as database

--- todo-list/Controllers/TodoListViewController.swift
+++ todo-list/Controllers/TodoListViewController.swift
@@ -9,20 +9,23 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = [
-        "Go grocery",
-        "Charge switch",
-        "Call grandparents"
-    ]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
+        let newItem = Item()
+        newItem.title = "IDK"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Item 2"
+        itemArray.append(newItem2)
+        
+        loadItems()
         
         let appearance = UINavigationBarAppearance()
         appearance.configureWithTransparentBackground()
@@ -39,18 +42,18 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -63,9 +66,11 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            self.itemArray.append(textField.text!)
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
-            self.tableView.reloadData()
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
         }
         
         alert.addTextField { alertTextField in
@@ -75,6 +80,30 @@ class TodoListViewController: UITableViewController {
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(self.itemArray)
+            try data.write(to: self.dataFilePath!)
+        } catch {
+            print("Error when encoding data, \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("error on decoding \(error)")
+            }
+            
+        }
     }
 }
 
---
+++ todo-list/Models/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  todo-list
+//
+//  Created by Yap Feng Yuan on 2022/02/06.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
