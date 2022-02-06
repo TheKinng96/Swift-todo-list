1ead89e0afe506ff54eb7cb4dd3c2453bb46637f
G3N <yap@corekara.co.jp>
2022-02-06 20:54:30 +0900

    added a checkmark on click event

--- todo-list/TodoListViewController.swift
+++ todo-list/TodoListViewController.swift
@@ -37,5 +37,16 @@ class TodoListViewController: UITableViewController {
         
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
